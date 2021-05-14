5fb5920cd6f7c5672fd95c1ac7f3d58221b4f827
RaphaelEscrig <raphael.escrig@hotmail.fr>
2021-05-14 15:18:09 +0200

    init

---
+++ config-overrides.js
@@ -0,0 +1,47 @@
+const {
+   override,
+   overrideDevServer,
+   addWebpackPlugin
+} = require("customize-cra");
+
+const CopyPlugin = require('copy-webpack-plugin');
+
+const multipleEntry = require('react-app-rewire-multiple-entry')([
+   {
+      entry: 'src/popup/index.tsx',
+      template: 'public/popup.html',
+      outPath: '/popup.html'
+   },
+   {
+      entry: 'src/options/index.tsx',
+      template: 'public/index.html',
+      outPath: '/index.html'
+   }
+]);
+
+const devServerConfig = () => config => {
+   return {
+      ...config,
+      writeToDisk: true
+   }
+}
+
+const copyPlugin = new CopyPlugin({
+   patterns: [
+      { from: 'public', to: '' },
+      { from: 'src/background.js', to: '' }
+   ]
+})
+
+module.exports = {
+   webpack: override(
+      addWebpackPlugin(
+         copyPlugin
+      ),
+      multipleEntry.addMultiEntry,
+   ),
+   devServer: overrideDevServer(
+      devServerConfig()
+   ),
+
+};
